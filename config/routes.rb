0dfbb942417ce9cc1cb37bb4ad4005f3e064b572
jcampbell57 <jcampbell57@gmail.com>
2023-10-04 12:13:29 -0700

    add about us page, implement bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # get 'home/index'
+  get 'home/about'
   root 'home#index'
 end
