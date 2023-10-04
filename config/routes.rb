3ac1bc6810b67e67d7ddf29b7545a69686f16c79
jcampbell57 <jcampbell57@gmail.com>
2023-10-04 15:03:16 -0700

    add friends scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :friends
   # get 'home/index'
   get 'home/about'
   root 'home#index'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 end
